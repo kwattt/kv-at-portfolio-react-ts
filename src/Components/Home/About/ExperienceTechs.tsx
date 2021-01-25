bc5fbec17bb5c5fcea3af95008b7327d44d5c532
kwat <jorgeglez1616@gmail.com>
2021-01-24 22:16:16 -0600

    Added english traductions

--- src/Components/Home/About/ExperienceTechs.tsx
+++ src/Components/Home/About/ExperienceTechs.tsx
@@ -8,46 +8,50 @@ import { DiReact } from 'react-icons/di';
 
 import Tech from './Tech'
 
+import {useTranslation} from 'react-i18next'
+
 export const CplusCase = () => {
+  const {t} = useTranslation()
   return (<>
     <HStack>
-      <Tech title="stdlib/boost" size={35} icon={VscLibrary} desc="Uso y comprensión de la misma" color="#72c550"/>
-      <Tech title="desktop apps" size={35} icon={CgScreen} desc="Aplicaciones simples de escritorio." color="#bed640"/>
+      <Tech title="stdlib/boost" size={35} icon={VscLibrary} desc={t('exp-cplus')} color="#72c550"/>
+      <Tech title="desktop apps" size={35} icon={CgScreen} desc={t('exp-desk')} color="#bed640"/>
     </HStack>
 
     <HStack mt={2}>
-      <Tech title="Game Modding" size={35} icon={BsController} desc="Múltiples juegos y distintas modificaciones." color="#72c550"/>
+      <Tech title="Game Modding" size={35} icon={BsController} desc={t('exp-game')} color="#72c550"/>
     </HStack>
   </>)
 }
 
 
 export const JSCase = () => {
+  const {t} = useTranslation()
   return (<>
     <HStack>
-      <Tech title="TypeScript" size={35} icon={SiTypescript} desc="Uso de Typescript" color="#72c550"/>
-      <Tech title="React" size={35} icon={DiReact} desc="Creación de distintas aplicaciones, webs." color="#72c550"/>
-      <Tech title="Vue.Js" size={35} icon={SiVueDotJs} desc="Creación de webs." color="#bed640"/>
+      <Tech title="TypeScript" size={35} icon={SiTypescript} desc={t('exp-ts')} color="#72c550"/>
+      <Tech title="React" size={35} icon={DiReact} desc={t('exp-react')} color="#72c550"/>
+      <Tech title="Vue.Js" size={35} icon={SiVueDotJs} desc={t('exp-vue')} color="#bed640"/>
     </HStack>
     <HStack mt={2}>
-      <Tech title="Express" size={35} icon={BsServer} desc="Servidores básicos en Express" color="#bed640"/>
+      <Tech title="Express" size={35} icon={BsServer} desc={t('exp-express')}  color="#bed640"/>
     </HStack>
   </>)
 }
 
 
 export const PythonCase = () => {
-
+  const {t} = useTranslation()
   return (<>
     <HStack>
-      <Tech title="Flask" size={35} icon={SiFlask} desc="Construcción de API, webs interactivas, uso de websockets." color="#72c550"/>
-      <Tech title="Numpy" size={35} icon={CgMathPlus} desc="Cálculos extensos y optimizados" color="#bed640"/>
-      <Tech title="Django" size={35} icon={SiDjango} desc="Construcción de Blog, libreta virtual" color="#bed640"/>
+      <Tech title="Flask" size={35} icon={SiFlask} desc={t('exp-flask')} color="#72c550"/>
+      <Tech title="Numpy" size={35} icon={CgMathPlus} desc={t('exp-numpy')} color="#bed640"/>
+      <Tech title="Django" size={35} icon={SiDjango} desc={t('exp-django')} color="#bed640"/>
     </HStack>
 
     <HStack mt={2}>
-    <Tech title="Matplotlib" size={35} icon={BsGraphDown} desc="Gráficas" color="#bed640"/>
-      <Tech title="Pygame" size={35} icon={BsController} desc="Juegos sencillos" color="#72c550"/>
+    <Tech title="Matplotlib" size={35} icon={BsGraphDown} desc={t('exp-plot')} color="#bed640"/>
+      <Tech title="Pygame" size={35} icon={BsController} desc={t('exp-pygame')} color="#72c550"/>
     </HStack>
 
   </>)
